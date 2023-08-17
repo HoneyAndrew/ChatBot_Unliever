3a6a8a7a7a9db857b0d604a9e28619c61e5611a2
nikita20002000 <vertihost@yandex.com>
2023-08-17 22:34:04 +0300

    Добавил скрипты для генерации картинки на основе трендов gpt_png выдает файл в формате png gpt_url выдает файл в формате json, в котором хранится ссылка на картинку

---
+++ db_ml/gpt_url_image_generator.py
@@ -0,0 +1,25 @@
+import openai
+import json
+
+
+openai_api_key = "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"
+openai.api_key = openai_api_key
+
+name_of_product = 'soap'
+prompt = f'hello, please generate for us a product card called {name_of_product} for marketplaces, which will look stylish and modern, add the product name and brand name there'
+
+
+response = openai.Image.create(
+
+    # Указываем сгенерированный текстовый промт
+    prompt=prompt,
+
+    # Указываем кол-во карточек, в нашем случае будем делать 5 разных
+    n=1,
+
+    # Указываем размер в пикселях
+    size='512x512',
+)
+
+with open("data.json", 'w') as file:
+    json.dump(response, file, indent=4, ensure_ascii=False)
